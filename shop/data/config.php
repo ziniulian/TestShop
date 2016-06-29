712668d7708d44651bd8c7df8795177867a103fb
ziniulian <ziniulian@163.com>
2016-06-29 22:34:10 +0800

    初始安装

---
+++ shop/data/config.php
@@ -0,0 +1,35 @@
+<?php
+// database host
+$db_host   = "127.3.206.2:3306";
+
+// database name
+$db_name   = "shop";
+
+// database username
+$db_user   = "shop";
+
+// database password
+$db_pass   = "123654";
+
+// table prefix
+$prefix    = "tsp_";
+
+$timezone    = "PRC";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '');
+
+?>
