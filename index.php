031583b232bcec1958fe40d93739c6e559a079e6
ziniulian <ziniulian@163.com>
2016-06-29 21:59:14 +0800

    未安装版

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hello World!</title>
+		<meta charset="utf-8" />
+
+		<style type="text/css">
+			html, body {
+				padding: 0;
+				margin: 0;
+			}
+		</style>
+
+		<script>
+			function init() {
+				console.log ("Hello World!");
+			}
+		</script>
+	</head>
+
+	<body>
+		<div>Hello World!</div>
+	</body>
+
+	<script type="text/javascript">
+		document.body.onload = init;
+	</script>
+</html>
